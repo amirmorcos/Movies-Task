7ff34fe5cbb2920c6258b769b9abec9aec27577b
amirmorcos <amirhmorcos@gmail.com>
2024-04-25 23:40:38 +0200

    installing packages

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,22 @@
 module.exports = {
-  presets: ['module:@react-native/babel-preset'],
+  presets: ["module:@react-native/babel-preset"],
+  plugins: [
+    [
+      "module-resolver",
+      {
+        alias: {
+          // This needs to be mirrored in tsconfig.json
+          atoms: "./src/components/atoms",
+          molecules: "./src/components/molecules",
+          organisms: "./src/components/organisms",
+          screens: "./src/screens",
+          navigation: "./src/navigation",
+          models: "./src/models",
+          store: "./src/store",
+          api: "./src/api",
+          themes: "./src/themes",
+        },
+      },
+    ],
+  ],
 };
